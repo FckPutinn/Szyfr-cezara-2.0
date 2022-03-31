3c73a1e1e4cd0e92116f3b6ce808c9265b18fb7d
3111233 <3111233@TE-GDA-D-D37H05>
2022-03-31 11:45:20 +0200

    Dodaj pliki projektów.

---
+++ Szyfr cezara 2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+string fraza;
+int klucz;
+Console.WriteLine("Podaj klucz szyfru:");
+klucz = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Podaj fraze do zaszyfrowania:");
+fraza = Console.ReadLine();
+char[] myChar = fraza.ToCharArray();
+for (int index = 0; index < myChar.Length; index++)
+{
+    myChar[index] = (char)(myChar[index] + klucz);
+        if (myChar[index] > 90)
+        {
+        myChar[index] = (char)((char)myChar[index] - 26 - 6);
+        }
+}
+string szyfr = new string(myChar);
+Console.WriteLine("Zaszyfrowana wiadomośc:");
+Console.WriteLine(szyfr);
